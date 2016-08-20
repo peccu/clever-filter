f5b2a59dc14e8a5ef257e05583fe6c34eeb7c7f5
peccu <peccul@gmail.com>
2016-08-20 14:46:21 +0900

    set default cllapsed to nav bar

--- client/components/navbar/navbar.component.js
+++ client/components/navbar/navbar.component.js
@@ -9,6 +9,7 @@ export class NavbarComponent {
     this.isLoggedIn = Auth.isLoggedInSync;
     this.isAdmin = Auth.isAdminSync;
     this.getCurrentUser = Auth.getCurrentUserSync;
+    this.isCollapsed = true;
   }
 
   isActive(route) {
