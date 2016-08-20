01b11304e17382807dc008d3359328c403149fa9
peccu <peccul@gmail.com>
2016-08-20 16:18:47 +0900

    update article components

--- client/app/articles/articles.component.js
+++ client/app/articles/articles.component.js
@@ -7,16 +7,29 @@ import routes from './articles.routes';
 
 export class ArticlesComponent {
   /*@ngInject*/
-  constructor() {
-    this.message = 'Hello';
+  constructor($http, $scope, socket) {
+    this.$http = $http;
+    this.socket = socket;
+
+    $scope.$on('$destroy', function() {
+      socket.unsyncUpdates('article');
+    });
   }
+
+  $onInit() {
+    this.$http.get('/api/articles')
+      .then(response => {
+        this.awesomeThings = response.data;
+        this.socket.syncUpdates('article', this.awesomeThings);
+      });
+  }
+
 }
 
 export default angular.module('cleverFilterApp.articles', [ngRoute])
   .config(routes)
   .component('articles', {
     template: require('./articles.html'),
-    controller: ArticlesComponent,
-    controllerAs: 'articlesCtrl'
+    controller: ArticlesComponent
   })
   .name;
